fcb7bc867aa8b2ba41ad53263af896d22540c7a8
Krytos <kmeinon@gmail.com>
2023-06-29 18:09:34 +0200

    Added readme and removed unneeded imports

---
+++ README.md
@@ -0,0 +1,55 @@
+# Tabata Timer
+
+This is a simple Tabata Timer I created for personal use. Feel free to use it as well. The application uses the `nicegui` library for creating a graphical user interface (GUI) and `asyncio` for handling asynchronous operations.
+
+![Tabata Timer](https://i.imgur.com/KIDzPB5.png)
+
+
+## Requirements
+
+- Python 3.x
+- nicegui library
+
+## Installation
+
+1. Make sure you have Python 3.x installed on your system.
+2. Install the nicegui library by running the following command:
+
+```shell
+pip install nicegui
+```
+
+3. Copy the code from this repository into your Python file.
+
+## Usage
+
+1. Run the Python file containing the code.
+2. The Tabata Timer application will open in a new window.
+3. Set the desired values for work time, rest time, and rounds using the input fields.
+4. Click the "Start" button to start the timer.
+5. The timer will display the minutes, seconds, and milliseconds remaining for each interval.
+6. The timer will automatically transition between work and rest intervals according to the specified times and number of rounds.
+7. Click the "Stop" button to stop the timer.
+8. To restart the timer with new settings, update the input fields and click "Start" again.
+
+## Code Explanation
+
+The code uses the `asyncio` library for handling asynchronous operations, allowing the timer to run without blocking the GUI.
+
+The `reset` function is responsible for resetting the timer labels and round counter.
+
+The `countdown` function is used to update the timer labels during each interval. It also plays audio files at specific times using the `ui.audio` function.
+
+The `main` function is the main logic of the timer. It starts or stops the timer based on the current state of the "Start" button. It iterates through the specified number of rounds and calls the `countdown` function for each work and rest interval.
+
+The code also includes the necessary GUI components for displaying the timer labels, input fields for setting the timer values, and the "Start" button.
+
+## Customization
+
+- You can modify the default values for work time, rest time, and rounds by changing the `value` attribute of the corresponding input fields.
+- Customize the GUI layout and styling by modifying the styles applied to the UI components.
+- Replace the audio files with your own by specifying the file paths in the `ui.audio` function calls.
+
+Feel free to modify the code according to your requirements and preferences.
+
+**Note:** Make sure to credit the `nicegui` library if you include this code or modified versions of it in your projects.
--- app.py
+++ app.py
@@ -1,70 +1,73 @@
-from rich import print
-from nicegui import app, ui
-import time
 import asyncio
+from nicegui import app, ui
+
 
 async def reset():
-	start_button.text = "Start"
-	minute_label.set_text("00:")
-	second_label.set_text("00")
-	milis_label.set_text(".0")
-	round_label.set_text(f"Rounds: 0 / {rounds.value}")
+    start_button.text = "Start"
+    minute_label.set_text("00:")
+    second_label.set_text("00")
+    milis_label.set_text(".0")
+    round_label.set_text(f"Rounds: 0 / {rounds.value}")
+
 
 async def countdown(t, type):
-	for i in range(t-1, -1, -1):
-		if start_button.text == "Start":			
-			return 
-		if i == 50:
-			ui.audio('countdown.wav', autoplay=True, controls=False)
-		if i == 10 and type == "rest":
-			ui.audio('work.mp3', autoplay=True, controls=False)
-		minute_label.set_text(f"{i//600:02d}:")
-		second_label.set_text(f"{i//10:02d}")
-		milis_label.set_text(f".{i%10}")
-		ui.update()		
-		await asyncio.sleep(0.1)
+    for i in range(t - 1, -1, -1):
+        if start_button.text == "Start":
+            return
+        if i == 50:
+            ui.audio("countdown.wav", autoplay=True, controls=False)
+        if i == 10 and type == "rest":
+            ui.audio("work.mp3", autoplay=True, controls=False)
+        minute_label.set_text(f"{i//600:02d}:")
+        second_label.set_text(f"{i//10:02d}")
+        milis_label.set_text(f".{i%10}")
+        ui.update()
+        await asyncio.sleep(0.1)
+
 
 async def main():
-	if start_button.text == "Start":
-		start_button.text = "Stop"
-		while True:		
-			w = int(work_time.value)*10
-			r = int(rest_time.value)*10
-			ro = int(rounds.value)	
-			for round in range(1, ro+1):
-				if start_button.text == "Start":			
-					break
-				round_label.set_text(f"Rounds: {round} / {ro}")
-				await countdown(w, "work")
-				await countdown(r, "rest")
-				if round == ro:
-					ui.audio('done.mp3', autoplay=True, controls=False)
-			await reset()
-			break		
-	else:
-		start_button.text = "Start"
+    if start_button.text == "Start":
+        start_button.text = "Stop"
+        while True:
+            w = int(work_time.value) * 10
+            r = int(rest_time.value) * 10
+            ro = int(rounds.value)
+            for round in range(1, ro + 1):
+                if start_button.text == "Start":
+                    break
+                round_label.set_text(f"Rounds: {round} / {ro}")
+                await countdown(w, "work")
+                await countdown(r, "rest")
+                if round == ro:
+                    ui.audio("done.mp3", autoplay=True, controls=False)
+            await reset()
+            break
+    else:
+        start_button.text = "Start"
 
 
-with ui.column().style('align-items: center; justify-content: center;'):
-	with ui.row().style('align-items: stretch; justify-content: center;'):
-		minute_label = ui.label("00:")
-		minute_label.style('font-size: 750%; font-weight: bold')
-		second_label = ui.label("00")
-		second_label.style('font-size: 750%; font-weight: bold; margin-left: -5%')
-		milis_label = ui.label(".0")
-		milis_label.style('font-size: 350%; font-weight: bold; align-self: end; margin-bottom: 7%; margin-left: -5%')
-	round_label = ui.label().style('font-size: 250%; font-weight: bold')
-	work_time = ui.input(label='Work', placeholder="20", value="20")
-	work_time.props('input-style="font-size: 150%; width: 80%; text-align: right; "')
-	rest_time = ui.input(label='Rest', placeholder="10", value="10")
-	rest_time.props('input-style="font-size: 150%; width: 80%; text-align: right"')
-	rounds = ui.input(label='Rounds', placeholder="8", value="8")
-	rounds.props('input-style="font-size: 150%; width: 80%; text-align: right"')
-	round_label.set_text(f"Rounds: 0 / {rounds.value}")
-	start_button = ui.button('Start', on_click=lambda: main())
-	start_button.style('width: 80%; height: 80%')
+with ui.column().style("align-items: center; justify-content: center;"):
+    with ui.row().style("align-items: stretch; justify-content: center;"):
+        minute_label = ui.label("00:")
+        minute_label.style("font-size: 750%; font-weight: bold")
+        second_label = ui.label("00")
+        second_label.style("font-size: 750%; font-weight: bold; margin-left: -5%")
+        milis_label = ui.label(".0")
+        milis_label.style(
+            "font-size: 350%; font-weight: bold; align-self: end; margin-bottom: 7%; margin-left: -5%"
+        )
+    round_label = ui.label().style("font-size: 250%; font-weight: bold")
+    work_time = ui.input(label="Work", placeholder="20", value="20")
+    work_time.props('input-style="font-size: 150%; width: 80%; text-align: right; "')
+    rest_time = ui.input(label="Rest", placeholder="10", value="10")
+    rest_time.props('input-style="font-size: 150%; width: 80%; text-align: right"')
+    rounds = ui.input(label="Rounds", placeholder="8", value="8")
+    rounds.props('input-style="font-size: 150%; width: 80%; text-align: right"')
+    round_label.set_text(f"Rounds: 0 / {rounds.value}")
+    start_button = ui.button("Start", on_click=lambda: main())
+    start_button.style("width: 80%; height: 80%")
 
 
-app.native.window_args['resizable'] = False
+app.native.window_args["resizable"] = False
 # app.native.start_args['debug'] = True
-ui.run(native=True,window_size=(400, 575), title="Tabata Timer", reload=False)
+ui.run(native=True, window_size=(400, 575), title="Tabata Timer", reload=False)
